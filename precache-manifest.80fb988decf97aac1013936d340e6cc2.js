f14e4861a5b3d507cbad2e12a603b4cf89711468
Maciej N <xyanteos@gmail.com>
2020-09-10 02:19:20 +0200

    Updates

---
+++ precache-manifest.80fb988decf97aac1013936d340e6cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3416b849bc5537c2fc16382340bf2ed9",
+    "url": "/REACT-site-project/index.html/index.html"
+  },
+  {
+    "revision": "d825b8374cb8840a727f",
+    "url": "/REACT-site-project/index.html/static/css/main.84a44ccb.chunk.css"
+  },
+  {
+    "revision": "46c52fa816628ea14b9e",
+    "url": "/REACT-site-project/index.html/static/js/2.54d0c32e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/REACT-site-project/index.html/static/js/2.54d0c32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d825b8374cb8840a727f",
+    "url": "/REACT-site-project/index.html/static/js/main.eaab911b.chunk.js"
+  },
+  {
+    "revision": "654a5ab61ecd69297d33",
+    "url": "/REACT-site-project/index.html/static/js/runtime-main.352980e4.js"
+  }
+]);
