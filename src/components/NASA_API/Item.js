190d270b94967c2f96bcaa92ebe9c88133ebb22a
Maciej N <xyanteos@gmail.com>
2020-09-10 03:06:53 +0200

    background and body visual changes

--- src/components/NASA_API/Item.js
+++ src/components/NASA_API/Item.js
@@ -5,7 +5,7 @@ const Item = (({item})=>{
     return(
     <div className="wyniki ui segment">
         <h1>{item.data ? item.data[0].title :null}</h1>
-        <img src={item.links ? item.links[0].href :null} alt={item.links ? item.links[0].rel : null}/>
+        <img src={item.links ? item.links[0].href :null} alt={item.links ? item.links[0].rel : "Image of the day"}/>
         <p>{item.data ? item.data[0].description : null}</p>
     </div>
     )
