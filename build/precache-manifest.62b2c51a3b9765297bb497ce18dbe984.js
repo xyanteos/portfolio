f29cfed0d62d9e90655d62360e48e0bde8d39a2c
Maciej N <xyanteos@gmail.com>
2020-09-10 02:28:00 +0200

    gogo

---
+++ build/precache-manifest.62b2c51a3b9765297bb497ce18dbe984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9586ec97d1ef2a0da0f61134ac032c",
+    "url": "/REACT-site-project/index.html"
+  },
+  {
+    "revision": "d825b8374cb8840a727f",
+    "url": "/REACT-site-project/static/css/main.84a44ccb.chunk.css"
+  },
+  {
+    "revision": "46c52fa816628ea14b9e",
+    "url": "/REACT-site-project/static/js/2.54d0c32e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/REACT-site-project/static/js/2.54d0c32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d825b8374cb8840a727f",
+    "url": "/REACT-site-project/static/js/main.eaab911b.chunk.js"
+  },
+  {
+    "revision": "ef36c427fe35f8ffdb89",
+    "url": "/REACT-site-project/static/js/runtime-main.22518de5.js"
+  }
+]);
