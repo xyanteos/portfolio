94d1a756d12594e7868a52c1795a22aa0605573e
unknown <xyanteos@gmail.com>
2024-01-05 15:52:20 +0100

    burger button menu behaviour with exit button changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [scrolled, setScrolled] = useState(false);
+  const [burgerActive, setBurgerActive] = useState(false);
 
   useEffect(() => {
     const handleScroll = () =>{
@@ -22,6 +23,11 @@ function App() {
   //   setScrolled(scrolled)
   // }
 
+  const HandleBurgerButtonClick = () =>{
+    setBurgerActive(!burgerActive);
+  }
+
+
   return (
     <div className={`Body`}>
       <header className={`HeaderContainer${scrolled ? ' sticky' : ''}`} >
@@ -38,12 +44,17 @@ function App() {
           <a className='AboutMe mobileHidden' href='/about'>O mnie</a>
           <a className='Projekty mobileHidden' href='/projects'>Projekty</a>
           <a className='Contact mobileHidden' href='/contact'>Kontakt</a>
-          <a className='burger'>
+          <a className={`burger${burgerActive ? ' active' : ''}`} onClick={HandleBurgerButtonClick}>
             <img src='./burger.svg'/>
           </a>
         </nav>
-        <nav className='NavBarMenu menu sidebar' style={{display: 'none'}}>
-          <a className='Logo' href='/'>
+        <nav className={`NavBarMenu menu sidebar${burgerActive ? '' : ' hidden'}`}>
+          <div className='exitButtonContainer'>
+            <a className={`exit${!burgerActive ? ' hidden' : ''}`} onClick={HandleBurgerButtonClick}>
+              <img src='./close.svg'/>
+            </a>
+          </div>
+          {/* <a className='Logo' href='/'>
             <span>
               Maciej
             </span>
@@ -51,7 +62,7 @@ function App() {
             <span>
               Nowacki
             </span>
-          </a>
+          </a> */}
           <a className='AboutMe' href='/about'>O mnie</a>
           <a className='Projekty' href='/projects'>Projekty</a>
           <a className='Contact' href='/contact'>Kontakt</a>
