f65895d1d91579f46afe719f5e5ab5030074ea96
unknown <xyanteos@gmail.com>
2024-01-13 06:22:37 +0100

    what is this favcon error

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import './components/Styles/style.css'
-
-ReactDOM.render(<App />,document.querySelector('#root'))
